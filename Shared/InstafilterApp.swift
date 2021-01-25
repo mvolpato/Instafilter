d8f61849b24c843ad4ab2b95796221e8cc465930
Michele Volpato <mvolpato@users.noreply.github.com>
2021-01-25 20:09:27 +0100

    Initial project

---
+++ Shared/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//  InstafilterApp
+//  Shared
+//
+//  Created by Michele Volpato on 25/01/2021.
+//  
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
